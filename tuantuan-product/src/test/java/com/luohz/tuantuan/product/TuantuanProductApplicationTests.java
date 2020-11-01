e8f98f4c366be8989a0ce1aab6fb4fc6860486ff
luohongzhang <luohongzhangzry@163.com>
2020-11-01 19:17:23 +0800

    完善团团商城的各个模块代码

--- tuantuan-product/src/test/java/com/luohz/tuantuan/product/TuantuanProductApplicationTests.java
+++ tuantuan-product/src/test/java/com/luohz/tuantuan/product/TuantuanProductApplicationTests.java
@@ -1,13 +1,23 @@
 package com.luohz.tuantuan.product;
 
+import com.luohz.tuantuan.product.entity.BrandEntity;
+import com.luohz.tuantuan.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TuantuanProductApplicationTests {
 
+
+	@Autowired
+	BrandService brandService;
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+		brandEntity.setName("小米");
+		brandService.save(brandEntity);
+		System.out.println("save success...");
 	}
 
 }
